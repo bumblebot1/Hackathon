f354a6cfc1a54ba00f8c0bef8e61234995bafddb
Oliver Crow <crowoy@gmail.com>
2017-02-27 19:01:00 +0000

    Temp and light sensors working. Web socket for server and client in NodeJS also working

---
+++ sensors/SensorServer.js
@@ -0,0 +1,33 @@
+var WebSocket = require("ws");
+var wss = new WebSocket.Server({port: 8080});
+
+var SerialPort = require("serialport");
+var serialPort = new SerialPort('/dev/cu.usbmodem1421', {
+    baudrate: 9600,
+    dataBits: 8, 
+    parity: 'none', 
+    stopBits: 1, 
+    flowControl: false 
+});
+
+var receivedData = "";
+
+serialPort.on("open", function () {
+    console.log('open serial communication');
+    serialPort.on('data', function(data) { 
+        receivedData += data.toString();
+        
+        if (receivedData .indexOf('}') >= 0 && receivedData .indexOf('{') >= 0) {
+            var sendData = receivedData .substring(receivedData .indexOf('{') + 1, receivedData .indexOf('}'));
+            receivedData = '';
+            console.log("{" + sendData + "}");
+            
+            // Broadcast to everyone else. 
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send("{" + sendData + "}");
+                }
+            });
+        }
+   });
+});
