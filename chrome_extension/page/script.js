b494efe6628fa183a62605ebbb95f2ded5b52a06
jy14898 <jy14898@my.bristol.ac.uk>
2017-02-28 01:50:02 +0000

    try

--- chrome_extension/page/script.js
+++ chrome_extension/page/script.js
@@ -6,89 +6,95 @@ for(var i = 0; i < 20; i++){
   data.push({light:0,temp:0});
 }
 
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function load_dat_graph() {
 
-var x = d3.scaleLinear()
-    .rangeRound([0, width]);
 
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
+  var svg = d3.select("svg"),
+      margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom,
+      g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var lineLight = d3.line()
-  .x(function(d,i) { return x(i); })
-  .y(function(d) { return y(d.light); });
+  var x = d3.scaleLinear()
+      .rangeRound([0, width]);
 
-var lineTemp = d3.line()
-  .x(function(d,i) { return x(i); })
-  .y(function(d) { return y(d.temp); });
+  var y = d3.scaleLinear()
+      .rangeRound([height, 0]);
 
-g.append("path").classed("light",true);
-g.append("path").classed("temp",true);
+  var lineLight = d3.line()
+    .x(function(d,i) { return x(i); })
+    .y(function(d) { return y(d.light); });
 
-// svg
-// .append('g')
-// .classed("xaxis",true)
-// .attr("transform", "translate("+margin.left+","+margin.top+")");
+  var lineTemp = d3.line()
+    .x(function(d,i) { return x(i); })
+    .y(function(d) { return y(d.temp); });
 
-svg
-.append('g')
-.classed("yaxis",true)
-.attr("transform", "translate("+margin.left+","+margin.top+")");
-
-ws.onmessage = function (event) {
-  var new_data = JSON.parse(event.data);
-  data.push(new_data);
-  data = data.slice(1,21);
-
-  x.domain(d3.extent(data, function(d,i) {return i; }));
-
-  var ycombined = [];
-  data.forEach(function(d){
-    ycombined.push(d.light);
-    ycombined.push(d.temp);
-  });
-
-  y.domain(d3.extent(ycombined, function(d) {return d; }));
-
-  // var xAxis = d3.axisBottom(x)
-  //     .ticks(20);
-
-  var yAxis = d3.axisLeft(y)
-      .ticks(5);  //
+  g.append("path").classed("light",true);
+  g.append("path").classed("temp",true);
 
   // svg
-  //   .select(".xaxis")
-  //   .call(xAxis);
+  // .append('g')
+  // .classed("xaxis",true)
+  // .attr("transform", "translate("+margin.left+","+margin.top+")");
 
   svg
-    .select(".yaxis")
-    .call(yAxis);
+  .append('g')
+  .classed("yaxis",true)
+  .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+  ws.onmessage = function (event) {
+    var new_data = JSON.parse(event.data);
+    data.push(new_data);
+    data = data.slice(1,21);
+
+    x.domain(d3.extent(data, function(d,i) {return i; }));
+
+    var ycombined = [];
+    data.forEach(function(d){
+      ycombined.push(d.light);
+      ycombined.push(d.temp);
+    });
+
+    y.domain(d3.extent(ycombined, function(d) {return d; }));
+
+    // var xAxis = d3.axisBottom(x)
+    //     .ticks(20);
+
+    var yAxis = d3.axisLeft(y)
+        .ticks(5);  //
+
+    // svg
+    //   .select(".xaxis")
+    //   .call(xAxis);
+
+    svg
+      .select(".yaxis")
+      .call(yAxis);
 
 
 
-  // g.append("g")
-  //     .attr("transform", "translate(0," + height + ")")
-  //     .call(d3.axisBottom(x))
+    // g.append("g")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x))
 
-  g.select(".light")
-    .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", lineLight);
+    g.select(".light")
+      .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", lineLight);
 
-  g.select(".temp")
-    .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", lineTemp);
-}
+    g.select(".temp")
+      .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", lineTemp);
+  }
+
+
+};
